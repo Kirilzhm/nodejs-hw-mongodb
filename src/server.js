72b7179833e0acffbd0283515eba9c0ba84be638
Kyrylo <kirilzhm@gmail.com>
2025-01-22 20:23:32 +0100

    the end of learning node.js

--- src/server.js
+++ src/server.js
@@ -32,17 +32,17 @@ export const setupServer = () => {
       });
     });
 
+    app.use('/uploads', express.static(UPLOAD_DIR));
+    
+    app.use('/api-docs', swaggerDocs());
+    
     app.use(router);
 
     app.use('*', notFoundHandler);
 
-    app.use('/uploads', express.static(UPLOAD_DIR));
-    
-    app.use('/api-docs', swaggerDocs());
-
     app.use(erroeHandler);
 
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+      console.log(`Server is running on port ${PORT}`);
+  });
 };
